18333339e90f69f0fd773dc6a14a4351ef7f41fd
MoRan <114865933+DuanMoRan@users.noreply.github.com>
2025-03-06 19:11:39 +0800

    Create SenderServer.java

---
+++ HomeworkOfJava/ToTheEnd/user-server/src/main/java/com/example/queues/SenderServer.java
@@ -0,0 +1,17 @@
+package com.example.queues;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Message;
+
+@Service
+public class SenderServer {
+    @Autowired
+    private RabbitTemplate template;
+
+    public void sendMessage(Message message){
+        template.convertAndSend("course-server", message);
+    }
+}
