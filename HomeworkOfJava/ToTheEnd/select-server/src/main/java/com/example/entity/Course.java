1585cee728ebe741d04d99255fc9f3bda7a8b57b
MoRan <114865933+DuanMoRan@users.noreply.github.com>
2025-03-06 19:10:21 +0800

    Create Course.java

---
+++ HomeworkOfJava/ToTheEnd/select-server/src/main/java/com/example/entity/Course.java
@@ -0,0 +1,15 @@
+package com.example.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import lombok.Data;
+
+@Data
+@Document(indexName = "course")
+public class Course {
+    @Id
+    private Integer courseNo;
+    private String courseName;
+    private String detail;
+}
