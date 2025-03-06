a8f370d53d0c02de1413954a86043af34bce8a2c
MoRan <114865933+DuanMoRan@users.noreply.github.com>
2025-03-06 19:11:07 +0800

    Create Course.java

---
+++ HomeworkOfJava/ToTheEnd/user-server/src/main/java/com/example/entity/Course.java
@@ -0,0 +1,11 @@
+package com.example.entity;
+
+import lombok.Data;
+
+@Data
+public class Course {
+    private Integer courseNo;
+    private String courseName;
+    private String detail;
+    private Integer remainder;
+}
