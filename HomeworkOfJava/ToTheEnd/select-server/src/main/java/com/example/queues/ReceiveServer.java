cec50f65934594a079c30a698ba9e3d9b42a8117
MoRan <114865933+DuanMoRan@users.noreply.github.com>
2025-03-06 19:10:24 +0800

    Create ReceiveServer.java

---
+++ HomeworkOfJava/ToTheEnd/select-server/src/main/java/com/example/queues/ReceiveServer.java
@@ -0,0 +1,32 @@
+package com.example.queues;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Course;
+import com.example.entity.Message;
+import com.example.service.SelectRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class ReceiveServer {
+
+    @Autowired
+    private SelectRepository repository;
+
+    @RabbitListener(queues = "select-server")
+    public void receiveMessage(Message message){
+        ObjectMapper objectMapper = new ObjectMapper();
+        Course course = objectMapper.convertValue(objectMapper, Course.class);
+        switch (message.getOpeartor()) {
+            case "c":
+                repository.save(course);
+                break;
+            case "d":
+                repository.deleteById(course.getCourseNo());
+                break;
+        }
+    }
+    
+}
