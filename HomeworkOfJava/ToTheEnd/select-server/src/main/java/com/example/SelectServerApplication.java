64bc6a3c7e5e23d101d0647f6ccd21b376cd6b05
MoRan <114865933+DuanMoRan@users.noreply.github.com>
2025-03-06 19:10:18 +0800

    Create SelectServerApplication.java

---
+++ HomeworkOfJava/ToTheEnd/select-server/src/main/java/com/example/SelectServerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelectServerApplication.class, args);
+	}
+
+}
