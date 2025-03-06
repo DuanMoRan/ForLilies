d598ef6085c8996e3a239061e349c395cb6580a1
MoRan <114865933+DuanMoRan@users.noreply.github.com>
2025-03-06 19:08:41 +0800

    Create ConfigRabbit.java

---
+++ HomeworkOfJava/ToTheEnd/course-server/src/main/java/com/example/config/ConfigRabbit.java
@@ -0,0 +1,27 @@
+package com.example.config;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableRabbit
+public class ConfigRabbit {
+
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+            Jackson2JsonMessageConverter jackson2JsonMessageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jackson2JsonMessageConverter);
+        return template;
+    }
+
+    @Bean
+    Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
