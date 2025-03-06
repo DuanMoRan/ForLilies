1461ce92c9361675c7b4b07d15e498b3a41465a9
MoRan <114865933+DuanMoRan@users.noreply.github.com>
2025-03-06 19:09:11 +0800

    Create ReceiverServer.java

---
+++ HomeworkOfJava/ToTheEnd/course-server/src/main/java/com/example/queues/ReceiverServer.java
@@ -0,0 +1,31 @@
+package com.example.queues;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.dao.CourseMapper;
+import com.example.entity.Course;
+import com.example.entity.Message;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class ReceiverServer {
+    @Autowired
+    private CourseMapper mapper;
+
+    @RabbitListener(queues = "course-server")
+    public void ReceiverMessage(Message message) {
+        // Course course = (Course) message.getData();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Course course = objectMapper.convertValue(message.getData(), Course.class);
+        switch (message.getOperator()) {
+            case "add":
+                mapper.add(course);
+                break;
+            case "decreas":
+                mapper.decreas(course);
+                break;
+        }
+    }
+}
