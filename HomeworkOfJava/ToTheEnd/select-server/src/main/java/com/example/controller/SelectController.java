7c1220f065ecae567bb3d013ee65739d55dd09cc
MoRan <114865933+DuanMoRan@users.noreply.github.com>
2025-03-10 17:28:12 +0800

    Update SelectController.java

--- HomeworkOfJava/ToTheEnd/select-server/src/main/java/com/example/controller/SelectController.java
+++ HomeworkOfJava/ToTheEnd/select-server/src/main/java/com/example/controller/SelectController.java
@@ -11,7 +11,7 @@ import com.example.entity.Total;
 import com.example.service.SelectService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.DeleteMapping;
 
 
 
@@ -20,12 +20,12 @@ public class SelectController {
     @Autowired
     private SelectService service;
 
-    @PostMapping("save")
-    public ResponseData save(@RequestBody Course course) {
-        if(service.save(course)) return new ResponseData(true, null, "添加成功");
-        else return new ResponseData(false, null, "添加失败");
+    // @PostMapping("save")
+    // public ResponseData save(@RequestBody Course course) {
+    //     if(service.save(course)) return new ResponseData(true, null, "添加成功");
+    //     else return new ResponseData(false, null, "添加失败");
         
-    }
+    // }
 
     @PostMapping("find/all")
     public ResponseData findAll(@RequestBody Total total) {
@@ -41,11 +41,11 @@ public class SelectController {
         else return new ResponseData(true, courses, "查询成功");
     }
 
-    @DeleteMapping("delete")
-    public ResponseData deleteById(@RequestBody Course course){
-        if(service.deleteById(course)) return new ResponseData(true, null, "删除成功");
-        else return new ResponseData(false, null, "删除失败");
-    }
+    // @DeleteMapping("delete")
+    // public ResponseData deleteById(@RequestBody Course course){
+    //     if(service.deleteById(course)) return new ResponseData(true, null, "删除成功");
+    //     else return new ResponseData(false, null, "删除失败");
+    // }
 
     @PostMapping("find/keywords")
     public ResponseData findByKeywords(@RequestBody Total total){
