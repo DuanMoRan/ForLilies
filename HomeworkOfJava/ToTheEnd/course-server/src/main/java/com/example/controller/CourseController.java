55b75cf763456a034b615022f1130ef2b00f2059
MoRan <114865933+DuanMoRan@users.noreply.github.com>
2025-03-24 19:13:31 +0800

    Update CourseController.java

--- HomeworkOfJava/ToTheEnd/course-server/src/main/java/com/example/controller/CourseController.java
+++ HomeworkOfJava/ToTheEnd/course-server/src/main/java/com/example/controller/CourseController.java
@@ -6,11 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entity.Course;
-import com.example.entity.CourseDTO;
-import com.example.entity.Message;
 import com.example.entity.Page;
 import com.example.entity.ResponseData;
-import com.example.queues.SenderServer;
 import com.example.service.CourseService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -23,9 +20,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 public class CourseController {
     @Autowired
     private CourseService service;
-
-    @Autowired
-    private SenderServer server;
 
     @PostMapping("find/all")
     public ResponseData findAll(@RequestBody Page page) {
@@ -48,34 +42,25 @@ public class CourseController {
 
     @PostMapping("create")
     public ResponseData createCourse(@RequestBody Course course) {
-        if (service.insertCourse(course)) {
-            send("c", course);
+        if (service.insertCourse(course))
             return new ResponseData(true, null, "创建成功");
-        } else
+        else
             return new ResponseData(false, null, "创建失败");
     }
 
     @PutMapping("update")
     public ResponseData updateCourse(@RequestBody Course course) {
-        if (service.updateCourse(course)) {
-            send("c", course);
+        if (service.updateCourse(course))
             return new ResponseData(true, null, "更新成功");
-        } else
+        else
             return new ResponseData(false, null, "更新失败");
     }
 
     @PostMapping("delete")
     public ResponseData deleteCourse(@RequestBody Course course) {
-        if (service.deleteCourse(course)) {
-            send("d", course);
+        if (service.deleteCourse(course))
             return new ResponseData(true, null, "删除成功");
-        } else
+        else
             return new ResponseData(false, null, "删除失败");
-    }
-
-    public void send(String operator, Course course) {
-        CourseDTO dto = new CourseDTO(course);
-        Message message = new Message(operator, dto);
-        server.sendMessage(message);
     }
 }
