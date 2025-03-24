448c58c543d59b2a3e39b6c6c67494f1c76577c2
MoRan <114865933+DuanMoRan@users.noreply.github.com>
2025-03-24 19:14:11 +0800

    Update StudentController.java

--- HomeworkOfJava/ToTheEnd/user-server/src/main/java/com/example/controller/StudentController.java
+++ HomeworkOfJava/ToTheEnd/user-server/src/main/java/com/example/controller/StudentController.java
@@ -6,27 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entity.Course;
-import com.example.entity.Message;
 import com.example.entity.ResponseData;
 import com.example.entity.SelectCourse;
 import com.example.entity.Student;
 import com.example.tool.JwtUtil;
 
-import com.example.queues.SenderServer;
 import com.example.service.StudentService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @RestController
-public class StudentController{
+public class StudentController {
 
     @Autowired
     private StudentService service;
-
-    @Autowired
-    private SenderServer server;
 
     @Autowired
     private JwtUtil jwtUtil;
@@ -34,64 +28,62 @@ public class StudentController{
     @PostMapping("space")
     public ResponseData findAll(@RequestBody Student student) {
         List<Student> students = service.find(student);
-        if(students.isEmpty()) return new ResponseData(false , students , "查询失败");
-        else return new ResponseData(true, students, "查询成功");
+        if (students.isEmpty())
+            return new ResponseData(false, students, "查询失败");
+        else
+            return new ResponseData(true, students, "查询成功");
     }
 
     @PostMapping("space/course")
     public ResponseData findCourse(@RequestBody Student student) {
         List<Course> courses = service.findCourses(student);
-        if(courses.isEmpty()) return new ResponseData(false , courses , "查询失败");
-        else return new ResponseData(true, courses , "查询成功");
+        if (courses.isEmpty())
+            return new ResponseData(false, courses, "查询失败");
+        else
+            return new ResponseData(true, courses, "查询成功");
     }
 
     @PostMapping("add/course")
     public ResponseData addCourse(@RequestBody SelectCourse course) {
-        if(service.addCourse(course)) {
-            send("decreas", course);
+        if (service.addCourse(course))
             return new ResponseData(true, null, "添加成功");
-        }
-        else return new ResponseData(false, null, "添加失败");
+        else
+            return new ResponseData(false, null, "添加失败");
     }
-    
+
     @PostMapping("dec/course")
     public ResponseData decCourse(@RequestBody SelectCourse course) {
-        if(service.decCourse(course)){
-            send("add", course);
+        if (service.decCourse(course))
             return new ResponseData(true, null, "移除成功");
-        }else return new ResponseData(false, null, "移除失败");
+        else
+            return new ResponseData(false, null, "移除失败");
     }
-    
 
     @PostMapping("login")
     public ResponseData login(@RequestBody Student student) {
         System.out.println(student);
-        if(service.login(student)){
+        if (service.login(student)) {
             String jwt = jwtUtil.generateToken(student.getStudentNo().toString());
             return new ResponseData(true, jwt, "登录成功");
-        }
-        else return new ResponseData(false, null, "登录失败");
+        } else
+            return new ResponseData(false, null, "登录失败");
     }
 
     @PutMapping("change/detail")
-    public ResponseData changeDetail(@RequestBody Student student){
+    public ResponseData changeDetail(@RequestBody Student student) {
 
-        if(service.change(student))return new ResponseData(true, null , "修改成功");
-        else return new ResponseData(false, null, "修改失败");
+        if (service.change(student))
+            return new ResponseData(true, null, "修改成功");
+        else
+            return new ResponseData(false, null, "修改失败");
     }
 
     @PutMapping("register")
     public ResponseData register(@RequestBody Student student) {
-        if(service.register(student)) return new ResponseData(true, null, "注册成功");
-        else return new ResponseData(false, null, "注册失败");
+        if (service.register(student))
+            return new ResponseData(true, null, "注册成功");
+        else
+            return new ResponseData(false, null, "注册失败");
     }
-    
-    public void send(String operator , SelectCourse course){
-        Message message = new Message();
-        message.setOperator(operator);
-        Course data = new Course();
-        data.setCourseNo(course.getCourseNo());
-        message.setData(data);
-        server.sendMessage(message);
-    }
+
 }
